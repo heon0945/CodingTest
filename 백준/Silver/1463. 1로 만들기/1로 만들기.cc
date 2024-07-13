46e159df91e21fb8be5b783da2846c75d89d0851
Heon <90744506+heon0945@users.noreply.github.com>
2024-07-14 02:19:58 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,29 +1,32 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-int DP[1000000];
-
 int main() {
-	int N;
-	cin >> N;
 
-	for (int i = 2; i <= N; i++) {
-		// 1. 1을 뺀다.
-		DP[i] = DP[i - 1] + 1;
+	int n;
+	cin >> n;
 
-		// 2. 2로 나누어지면
-		if (i % 2 == 0) {
-			DP[i] = min(DP[i], DP[i / 2] + 1);
-		}
+	//dp
+	//1.	3으로 나누어 떨어지면 /3	-> *3
+	//2.	2로 나누어 떨어지면 /2		-> *2
+	//3.	-1							-> +1
 
-		// 3. 3으로 나누어지면
-		if (i % 3 == 0) {
-			DP[i] = min(DP[i], DP[i / 3] + 1);
-		}
+	int dp[1000000];
+
+	dp[1] = 0;
+	for (int i = 2; i <= n; i++) {
+		//1.
+		dp[i] = dp[i - 1] + 1;
+		//2.
+		if (i % 2 == 0)
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		//3.
+		if (i % 3 == 0)
+			dp[i] = min(dp[i], dp[i / 3] + 1);
 	}
 
-	cout << DP[N];
+	cout << dp[n];
 
 	return 0;
 }
