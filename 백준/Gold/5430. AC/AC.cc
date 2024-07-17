077e5d17b036a12b87fce6acb6aa922eca64161f
Heon <90744506+heon0945@users.noreply.github.com>
2024-07-17 12:25:11 +0900

    [Gold V] Title: AC, Time: 104 ms, Memory: 3312 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.cc
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <deque>
+
+using namespace std;
+
+
+deque<int> read(string str) {
+	deque<int> input;
+	string s = "";
+	for (char c : str) {
+		if (isdigit(c)) {
+			s += c;
+		}
+		else {
+			if (!s.empty()) {
+				input.push_back(stoi(s));
+				s = "";
+			}
+		}
+	}
+
+	return input;
+}
+
+
+int main() {
+
+	int tc;
+	string op;
+	int n;
+
+
+	cin >> tc;
+
+	while (tc--) {
+
+		cin >> op;
+
+		cin >> n;
+
+		string str;
+		cin >> str;
+
+		deque<int> input = read(str);
+
+		bool flag = false;
+		bool order = true;
+
+		for (char c : op) {
+			if (c == 'R') {
+				if (order)
+					order = false;
+				else
+					order = true;
+			}
+			else if (c == 'D') {
+				if (input.empty()) {
+					cout << "error" << '\n';
+					flag = true;
+					break;
+				}
+				if (order)
+					input.pop_front();
+				else
+					input.pop_back();
+			}
+		}
+
+		/*if (flag)
+			cout << "error" << '\n';
+		else {
+			if (input.empty()) {
+				cout << "[]";
+			}
+
+			else {
+				if (!order)
+					reverse(input.begin(), input.end());
+				cout << "[";
+				for (int i = 0; i < input.size() - 1; i++) {
+					cout << input[i] << ", ";
+				}
+				cout << input[input.size() - 1] << "]" << '\n';
+			}
+		}*/
+		if (!flag) {
+			cout << '[';
+		}
+		if (!order && !input.empty()) {
+			for (auto o = input.rbegin(); o != input.rend(); o++) {
+				if (o == input.rend() - 1)
+					cout << *o;
+				else
+					cout << *o << ',';
+			}
+		}
+		else if (order && !input.empty()) {
+			for (auto o = input.begin(); o != input.end(); o++) {
+				if (o == input.end() - 1)
+					cout << *o;
+				else
+					cout << *o << ',';
+			}
+		}
+		if (!flag)
+			cout << "]\n";
+
+
+	}
+
+	return 0;
+}
