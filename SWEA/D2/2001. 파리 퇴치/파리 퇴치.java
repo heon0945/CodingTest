da0d061a238a9d9b3bfbc8f5316d134af50787d0
Heon <90744506+heon0945@users.noreply.github.com>
2024-08-14 15:13:39 +0900

    [D2] Title: 파리 퇴치, Time: 131 ms, Memory: 19,208 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,14 +1,17 @@
 import java.io.*;
 import java.util.*;
 
+// 18,724 kb, 118 ms
+
 public class Solution {
 
-	//nxn 배열 생성, 가로 세로 0~n-m까지 탐색
-	//현재 자신이 왼쪽 꼭짓점이라 생각 -> 좌우로 0~m-1 정사각형 탐색 -> 파리 개수 업데이트
-	//파리 개수가 현재 최댓값보다 큰 경우 정답 업데이트
+	//nxn 배열 생성, 배열의 값을 축적하는 배열 생성 
+	//가로 세로 m-1~n-1까지 탐색
+	//파리 수 확인하면서 최댓값 업데이트
 	
 	static int tc, n, m;
 	static int[][] board;
+	static int[][] accum;
 	
 	public static void main(String[] args) throws IOException {
 		StringBuilder sb = new StringBuilder();
@@ -22,7 +25,9 @@ public class Solution {
 			n = Integer.parseInt(st.nextToken());
 			m = Integer.parseInt(st.nextToken());
 			
+			//make basic array
 			board = new int[n][n];
+			accum = new int[n][n];
 			for(int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
 				for(int j = 0; j < n; j++) {
@@ -30,18 +35,35 @@ public class Solution {
 				}
 			}
 			
-			for(int i = 0; i <= n-m; i++) {
-				for(int j = 0; j <= n-m; j++) {
-					int flies = 0;
-					for(int w = i; w <= i + m-1; w++) {
-						for(int h = j; h <= j + m-1; h++) {
-							flies += board[w][h];
-						}
-					}
+			//make accumulation array
+			accum[0][0] = board[0][0];
+			for(int i = 1; i < n; i++) {
+				accum[0][i] = accum[0][i-1] + board[0][i];
+				accum[i][0] = accum[i-1][0] + board[i][0];
+			}			
+			for(int i = 1; i < n; i++) {
+				for(int j = 1; j < n; j++) {
+					accum[i][j] = board[i][j] + accum[i-1][j] + accum[i][j-1] - accum[i-1][j-1];
+				}
+			}
+			
+			//search
+			int flies = 0;
+			for(int i = m-1; i < n; i++) {
+				for(int j = m-1; j < n; j++) {
+					flies = accum[i][j];
+					if(i-m >= 0)
+						flies -= accum[i-m][j];
+					if(j-m >= 0)
+						flies -= accum[i][j-m];
+					if(i-m >= 0 && j-m >= 0)
+						flies += accum[i-m][j-m];
 					if(answer < flies)
 						answer = flies;
 				}
 			}
+			
+			
 			sb.append("#").append(t).append(" ").append(answer).append('\n');
 		}
 		
