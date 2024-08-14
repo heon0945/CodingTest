7b134a3545be2c34d5e346ee812d31a77b069d8d
Heon <90744506+heon0945@users.noreply.github.com>
2024-08-14 15:18:08 +0900

    [D2] Title: 파리 퇴치, Time: 116 ms, Memory: 18,532 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -26,38 +26,27 @@ public class Solution {
 			m = Integer.parseInt(st.nextToken());
 			
 			//make basic array
-			board = new int[n][n];
-			accum = new int[n][n];
-			for(int i = 0; i < n; i++) {
+			board = new int[n+1][n+1];
+			accum = new int[n+1][n+1];
+			for(int i = 1; i <= n; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j = 0; j < n; j++) {
+				for(int j = 1; j <= n; j++) {
 					board[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			//make accumulation array
-			accum[0][0] = board[0][0];
-			for(int i = 1; i < n; i++) {
-				accum[0][i] = accum[0][i-1] + board[0][i];
-				accum[i][0] = accum[i-1][0] + board[i][0];
-			}			
-			for(int i = 1; i < n; i++) {
-				for(int j = 1; j < n; j++) {
+			//make accumulation array		
+			for(int i = 1; i <= n; i++) {
+				for(int j = 1; j <= n; j++) {
 					accum[i][j] = board[i][j] + accum[i-1][j] + accum[i][j-1] - accum[i-1][j-1];
 				}
 			}
 			
 			//search
 			int flies = 0;
-			for(int i = m-1; i < n; i++) {
-				for(int j = m-1; j < n; j++) {
-					flies = accum[i][j];
-					if(i-m >= 0)
-						flies -= accum[i-m][j];
-					if(j-m >= 0)
-						flies -= accum[i][j-m];
-					if(i-m >= 0 && j-m >= 0)
-						flies += accum[i-m][j-m];
+			for(int i = m; i <= n; i++) {
+				for(int j = m; j <= n; j++) {
+					flies = accum[i][j] - accum[i-m][j] - accum[i][j-m] + accum[i-m][j-m];
 					if(answer < flies)
 						answer = flies;
 				}
