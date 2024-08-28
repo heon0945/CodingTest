a46b6e0c450c40ce7b45b7061fe9cb94980adb18
Heon <90744506+heon0945@users.noreply.github.com>
2024-08-29 00:06:51 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 171 ms, Memory: 28,544 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -1,23 +1,23 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 
 public class Solution {
 	//10개의 테스트 케이스
-	//인덱스 주의
-	//데이터 길이, 시작점
-	//데이터(from, to)
-	//최대 연락인원 100명 -> 100x100 인접행렬 생성
-	//데이터 받으면서 데이터 길이 /2 반복 : from, to -> 행렬 쌍으로 인접행렬에 기록
-	//방문체크
-	//시작점부터 bfs 실행 (파라메터 : 현재 번호)
-		//큐에 시작점 번호 삽입
-		//시작점의 행을 살펴보면서 1인 경우 큐에 삽입
-		//각 bfs 단계마다 최댓값 기록
-	//bfs 끝나면 기록한 최댓값 출력
-	
+		//인덱스 주의
+		//데이터 길이, 시작점
+		//데이터(from, to)
+		//최대 연락인원 100명 -> 100개의 인접리스트 생성
+		//데이터 받으면서 데이터 길이 /2 반복 :  from, to -> from에 해당하는 인접리스트에 추가
+		//방문체크
+		//시작점부터 bfs 실행 (파라메터 : 현재 번호)
+			//큐에 시작점 번호 삽입
+			//시작점의 리스트를 살펴보면서 1인 경우 큐에 삽입
+			//각 bfs 단계마다 최댓값 기록
+		//bfs 끝나면 기록한 최댓값 출력
 	static int data;
 	static int start;
-	static boolean map[][];
+	static ArrayList<ArrayList<Integer>> list;
 	static boolean visit[];
 	static int answer;
 	
@@ -36,7 +36,10 @@ public class Solution {
 		StringBuilder sb = new StringBuilder();
 		
 		int tc = 1;
-		map = new boolean[101][101];
+		list = new ArrayList<ArrayList<Integer>>();
+		for(int i = 0; i < 101; i++) {
+			list.add(new ArrayList<Integer>());
+		}
 		visit = new boolean[101];
 		
 		while(tc <= 10) {
@@ -49,7 +52,7 @@ public class Solution {
 				int from = sc.nextInt();
 				int to = sc.nextInt();
 				
-				map[from][to] = true;
+				list.get(from).add(to);
 			}
 			
 			answer = -1;
@@ -59,7 +62,7 @@ public class Solution {
 			
 			//초기화
 			for(int i = 0; i < 101; i++) {
-				Arrays.fill(map[i], false);
+				list.get(i).clear();
 				visit[i] = false;
 			}
 			
@@ -90,13 +93,12 @@ public class Solution {
 			}
 			
 			//추가
-			for(int i = 1; i <= 100; i++) {
-				if(map[cur.num][i] && !visit[i]) {
+			for(int i : list.get(cur.num)) {
+				if(!visit[i]) {
 					q.add(new Node(i, cur.len + 1));
 					visit[i] = true;
 				}
 			}
 		}
 	}
-	
 }
