9f228faa6ad8bae5a0a36c4981671970df6bd178
Heon <90744506+heon0945@users.noreply.github.com>
2024-09-06 11:03:48 +0900

    [D4] Title: [Professional] 키 순서, Time: 548 ms, Memory: 92,428 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -27,89 +27,52 @@ public class Solution {
 				adjMat[a][b] = 1;
 			}
 			
-			
 			for(int i = 1; i <= n; i++) {
-				cnt = 0;
-				//bfs
-//				if(gtBFS(i) + ltBFS(i) == n-1) {
-//					answer++;
-//				}	
-				//dfs
-				gtDFS(i, new boolean[n+1]);
-				ltDFS(i, new boolean[n+1]);
-				if(cnt == n-1) answer++;
-				
+				adjMat[i][0] = -1;  //탐색되지 않은 학생을 나타냄 (탐색 완료 시 자신보다 큰 학생의 수를 저장)
 			}
 			
+			
+			for(int i = 1; i <= n; i++) {
+				if(adjMat[i][0] != -1) continue;
+				gtDFS(i);
+			}
+			
+			//자신보다 작은 정점의 개수도 카운트
+			for(int i = 1; i <= n; i++) {
+				for(int j = 1; j <= n; j++) {
+					adjMat[0][j] += adjMat[i][j];
+				}
+			}
+			
+			for(int k = 1; k <= n; k++) {
+				if(adjMat[k][0] + adjMat[0][k] == n-1)
+					answer++;
+			}
 			System.out.println("#" + t + " " + answer);
 		}
 	}
 	
-	private static void gtDFS(int cur, boolean[] visit) {
-		
-		visit[cur] = true;
+	private static void gtDFS(int cur) {
 		
 		for(int i = 1; i <= n; i++) {
-			if(visit[i]) continue;
-			if(adjMat[cur][i] != 0) {
-				gtDFS(i, visit);
-				cnt++;
+			if(adjMat[cur][i] == 0) continue; //관계 없는 정점은 패스 (나보다 큰 정점만 탐색)
+			if(adjMat[i][0] == -1) { //아직 탐색 안 된 정점은 탐색 실행
+				gtDFS(i);
 			}
-		}
-	}
-	
-private static void ltDFS(int cur, boolean[] visit) {
-		
-		visit[cur] = true;
-		
-		for(int i = 1; i <= n; i++) {
-			if(visit[i]) continue;
-			if(adjMat[i][cur] != 0) {
-				ltDFS(i, visit);
-				cnt++;
-			}
-		}
-	}
-	
-	private static int gtBFS(int start) {
-		Queue<Integer> q = new ArrayDeque<>();
-		boolean visit[] = new boolean[n+1];
-		
-		q.add(start);
-		visit[start] = true;
-		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			for(int i = 1; i <= n ; i++) {
-				if(visit[i]) continue;
-				if(adjMat[cur][i] != 0) {
-					q.add(i);
-					visit[i] = true;
-					cnt++;
+			//이미 탐색되어 정보가 저장된 정점
+			if(adjMat[i][0] > 0) { //해당 정점보다 키가 큰 정점이 있다면 그 정보를 반영
+				for(int j = 1; j <= n; j++) {
+					if(adjMat[i][j] != 0)
+						adjMat[cur][j] = 1; //현재 정점에도 업데이트
 				}
 			}
 		}
-		return cnt;
+		
+		//자신보다 큰 정점의 정보를 전부 반영한 경우 (탐색 끝난 경우)
+		adjMat[cur][0] = 0;
+		for(int k = 1; k <= n; k++) {
+			adjMat[cur][0] += adjMat[cur][k]; //키가 크고 작은 경우를 0과 1로만 표현하고 있기 때문에 결국 키가 큰 애들의 개수가 저장됨
+		}
 	}
 	
-	private static int ltBFS(int start) {
-		Queue<Integer> q = new ArrayDeque<>();
-		boolean visit[] = new boolean[n+1];
-		
-		q.add(start);
-		visit[start] = true;
-		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			for(int i = 1; i <= n ; i++) {
-				if(visit[i]) continue;
-				if(adjMat[i][cur] != 0) {
-					q.add(i);
-					visit[i] = true;
-					cnt++;
-				}
-			}
-		}
-		return cnt;
-	}
 }
