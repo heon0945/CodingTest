68c99ef8865efc1b0152afe4749f1aae9ac813f0
Heon <90744506+heon0945@users.noreply.github.com>
2024-09-08 16:53:47 +0900

    [D4] Title: 물놀이를 가자, Time: 1,008 ms, Memory: 113,172 KB -BaekjoonHub

--- SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
@@ -5,90 +5,82 @@ public class Solution {
 	static int tc;
 	static int n, m;
 	static char map[][];
-	static int dist[][];
-	static int dx[] = { -1, 0, 1, 0 };
-	static int dy[] = { 0, 1, 0, -1 };
-	static int answer;
+	static int ans;
+	static boolean visit[][];
+	static int dx[] = {-1, 0, 1, 0};
+	static int dy[] = {0, -1, 0, 1};
+	
+	
+	static class Pair{
+		int x, y;
 
-	static class Node {
-		int x, y, level;
-
-		public Node(int x, int y, int level) {
+		public Pair(int x, int y) {
 			super();
 			this.x = x;
 			this.y = y;
-			this.level = level;
 		}
 	}
-
+	
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-
+		
 		tc = Integer.parseInt(br.readLine());
-
-		for (int t = 1; t <= tc; t++) {
+		
+		for(int t = 1; t <= tc; t++) {
 			sb.append("#").append(t).append(" ");
-			st = new StringTokenizer(br.readLine());
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			m = Integer.parseInt(st.nextToken());
-			answer = 0;
-
+			
+			ans = 0;
 			map = new char[n][m];
-			dist = new int[n][m];
-
-			Queue<Node> q = new ArrayDeque<>();
-
-			for (int i = 0; i < n; i++) {
-				String str = br.readLine();
-				for (int j = 0; j < m; j++) {
-					map[i][j] = str.charAt(j);
-					dist[i][j] = Integer.MAX_VALUE;
-
-					if (map[i][j] == 'W') {
-						q.add(new Node(i, j, 0));
-						dist[i][j] = 0;
+			visit = new boolean[n][m];
+			Queue<Pair> q = new ArrayDeque<>(); 
+			
+			for(int i = 0; i < n; i++) {
+				String tmp = br.readLine();
+				for(int j = 0; j < m; j++) {
+					map[i][j] = tmp.charAt(j);
+					if(map[i][j] == 'W') {
+						q.add(new Pair(i, j));
+						visit[i][j] = true;
 					}
 				}
 			}
-
-			while (!q.isEmpty()) {
-				Node cur = q.poll();
-				int curx = cur.x;
-				int cury = cur.y;
-				int curlv = cur.level;
-				
-				for (int i = 0; i < 4; i++) {
-					int tx = curx + dx[i];
-					int ty = cury + dy[i];
-
-					if (OOB(tx, ty))
-						continue;
-					if (dist[tx][ty] <= curlv + 1)
-						continue;
-					dist[tx][ty] = curlv + 1;
-					q.add(new Node(tx, ty, curlv + 1));
+			
+			//bfs
+			int dist = 0;
+			while(!q.isEmpty()) {
+				int sz = q.size();
+				dist++;
+				while(sz > 0) {
+					Pair cur = q.poll();
+					for(int i = 0; i < 4; i++) {
+						int tx = cur.x + dx[i];
+						int ty = cur.y + dy[i];
+						
+						if(OOB(tx, ty)) continue;
+						if(visit[tx][ty]) continue;
+						if(map[tx][ty] == 'L') {
+							visit[tx][ty] = true;
+							q.add(new Pair(tx, ty));
+							ans += dist;
+						}
+					}
+					sz--;
 				}
 			}
 			
-			// L칸의 거리 합 구하기
-
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if (map[i][j] == 'L')
-						answer += dist[i][j];
-				}
-			}
-
-			sb.append(answer).append('\n');
-
+			sb.append(ans).append('\n');
 		}
 		System.out.println(sb);
+		
 	}
-
+	
 	static boolean OOB(int x, int y) {
 		return x < 0 || x >= n || y < 0 || y >= m;
 	}
+	
 }
