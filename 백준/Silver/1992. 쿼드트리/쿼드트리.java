862914c93cde4f98c0e13a91541df125355ebe6a
Heon <90744506+heon0945@users.noreply.github.com>
2025-07-01 17:05:13 +0900

    [Silver I] Title: 쿼드트리, Time: 68 ms, Memory: 12084 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,51 +1,82 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n;
-	static int[][] map;
-	static String answer = "";
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
-		n = Integer.parseInt(br.readLine());
-		map = new int[n][n];
-		for(int i = 0; i < n; i++) {
-			String str = br.readLine();
-			for(int j = 0; j < n; j++) {
-				map[i][j] = str.charAt(j) - '0';
-			}
-		}
-	
-		search(0, 0, n);
-		System.out.print(answer);
-	}
-	
-	static void search(int r, int c, int size) {
-		int sum = 0;
-		for(int x = r; x < r + size; x++) {
-			for(int y = c; y < c + size; y++) {
-				sum += map[x][y];
-			}
-		}
-	
-		if(sum == 0) {
-			answer += "0";
-		}
-		else if(sum == size * size) {
-			answer += "1";
-		}
-		else {
-			int half = size / 2;
-			answer += "(";
-			search(r, c, half);
-			search(r, c + half, half);
-			search(r + half, c, half);
-			search(r + half, c + half, half);
-			answer += ")";
-		}
-	
-	}
+
+    static int N;
+    static int map[][];
+
+
+    static StringBuilder sb;
+
+    public static class Pair{
+        int x, y;
+        public Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        //맵 만들기
+        for(int i = 0; i < N; i++){
+            String str = br.readLine();
+            for(int j = 0; j < N; j++){
+                map[i][j] = str.charAt(j)-'0';
+            }
+        }
+
+        sb = new StringBuilder();
+
+
+        qtree(new Pair(0, 0), new Pair(N, N));
+
+        System.out.println(sb);
+    }
+
+
+    public static void qtree(Pair start, Pair end){
+
+        int pivot = map[start.x][start.y];
+        boolean valid = true;
+
+        //현재 종료조건(범위가 1인경우)
+        if(end.x-start.x == 1){
+            sb.append(pivot);
+            return;
+        }
+
+
+        //현재 영상이 하나로 표현 가능한지
+        for(int i = start.x; i < end.x; i++){
+            for(int j = start.y; j < end.y; j++){
+                if(map[i][j] != pivot) {
+                    valid = false;
+                    break;
+                }
+            }
+            if(!valid) break;
+        }
+
+        //하나의 영상으로 표현 가능
+        if(valid){
+            sb.append(pivot);
+            return;
+        }
+
+        //하나의 영상으로 표현 불가능
+        int range = (end.x-start.x) / 2;
+        sb.append('(');
+        qtree(new Pair(start.x, start.y), new Pair(start.x + range, start.y + range));
+        qtree(new Pair(start.x, start.y + range), new Pair(start.x + range, end.y));
+        qtree(new Pair(start.x + range, start.y), new Pair(end.x, start.y + range));
+        qtree(new Pair(start.x + range, start.y + range), new Pair(end.x, end.y));
+        sb.append(')');
+
+    }
 }
