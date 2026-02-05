fb70ca7814b9e612e7170a32d86c7237471e5274
Heon <90744506+heon0945@users.noreply.github.com>
2026-02-05 21:34:05 +0900

    Time: 315 ms (91.77%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,23 @@
+WITH date_tb AS
+(
+    SELECT visited_on, SUM(amount) AS date_amount
+    FROM Customer
+    GROUP BY visited_on
+),
+total_tb AS
+(
+    SELECT visited_on, SUM(date_amount) OVER (ORDER BY visited_on) AS total_amount
+    FROM date_tb
+),
+sevenday_tb AS
+(
+    SELECT t1.visited_on, t1.total_amount - IFNULL(t2.total_amount, 0)  AS amount
+    FROM total_tb t1
+    LEFT JOIN total_tb t2
+    ON t2.visited_on = DATE_ADD(t1.visited_on, INTERVAL -7 DAY)
+    WHERE (SELECT MIN(visited_on) FROM total_tb) + 6 <= t1.visited_on 
+)
+
+SELECT visited_on, amount, ROUND(amount / 7, 2) AS average_amount
+FROM sevenday_tb
+ORDER BY visited_on
